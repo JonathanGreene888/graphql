c78736686b9d683d12da5a2e0755726ccce3720d
Jonathan Greene <jonathan.greene@autozone.com>
2019-07-31 11:15:04 -0400

    cleanred up type file

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,33 +1,23 @@
-import { find, filter } from "lodash";
-import { authors, masterRuleContent, posts, majors, rules } from "./data";
+import { find } from 'lodash';
+import { masterRuleContent, posts, majors, rules } from './data';
 
 const resolvers = {
-  Query: {
-    posts: () => posts,
-    author: (_, { id }) => find(authors, { id }),
-    getMajors: () => majors,
-    getMasterRuleContent: () => masterRuleContent,
-    getCalculationRulesByMajorID: (_, { majorID }) => rules
-  },
+    Query: {
+        getMajors: () => majors,
+        getMasterRuleContent: () => masterRuleContent,
+        getCalculationRulesByMajorID: (_, { majorID }) => rules,
+    },
 
-  Mutation: {
-    upvotePost: (_, { postId }) => {
-      const post = find(posts, { id: postId });
-      if (!post) {
-        throw new Error(`Couldn't find post with id ${postId}`);
-      }
-      post.votes += 1;
-      return post;
-    }
-  },
-
-  Author: {
-    posts: author => filter(posts, { authorId: author.id })
-  },
-
-  Post: {
-    author: post => find(authors, { id: post.authorId })
-  }
+    Mutation: {
+        upvotePost: (_, { postId }) => {
+            const post = find(posts, { id: postId });
+            if (!post) {
+                throw new Error(`Couldn't find post with id ${postId}`);
+            }
+            post.votes += 1;
+            return post;
+        },
+    },
 };
 
 export default resolvers;

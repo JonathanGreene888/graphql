34bf4e1507c4563c715904b6dc067f01320b3f89
Daniel Yao <daniel.yao@autozone.com>
2019-08-04 14:16:19 -0400

    updated the mutation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
+import express from "express";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
 
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 // The GraphQL schema in string form
-import typeDefs from './types';
+import typeDefs from "./types";
 // The GraphQL resolvers
-import resolvers from './resolvers';
+import resolvers from "./resolvers";
 
 const cors = require(`cors`);
 
@@ -18,22 +18,21 @@ const app = express();
 
 app.use(cors());
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
 // GraphiQL, a visual editor for queries
 app.use(
-    '/graphiql',
-    graphiqlExpress({
-        endpointURL: '/graphql',
-        query: `# Welcome to GraphiQL
+  "/graphiql",
+  graphiqlExpress({
+    endpointURL: "/graphql",
+    query: `# Welcome to GraphiQL
         mutation updateWeeksOfDemandRule($input: WeeksOfDemandInput) {
-          updateWeeksOfDemandRule(input: $input) {
-          }
-        }`,
-    })
+          updateWeeksOfDemandRule(input: $input)
+        }`
+  })
 );
 
 // Start the server
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Go to ${process.env.PORT || 3000} to run queries!`);
+  console.log(`Go to ${process.env.PORT || 3000} to run queries!`);
 });
